992f0a53a1f44706bdb60619853df886f33bc89d
Tom Andrews <git@tomandrews.co.uk>
2013-03-10 18:54:23 +0000

    Simple numbers, now work.

--- roman_numeral_generator.rb
+++ roman_numeral_generator.rb
@@ -1,5 +1,21 @@
 class RomanNumeralGenerator
+	ROMAN_NUMERALS = {
+		1 => "I",
+		4 => "IV",
+		5 => "V",
+		9 => "IX",
+		10 => "X",
+		40 => "XL",
+		50 => "L",
+		90 => "XC",
+		100 => "C",
+		400 => "CD",
+		500 => "D",
+		900 => "CM",
+		1000 => "M"
+	}
+	
 	def self.generate (to_convert)
-		return to_convert
+		return ROMAN_NUMERALS[to_convert]
 	end
 end
--- roman_numeral_generator_test.rb
+++ roman_numeral_generator_test.rb
@@ -5,5 +5,17 @@ class RomanNumeralGeneratorTest < Test::Unit::TestCase
 
 	def test_simplest_values
 		assert_equal("I", RomanNumeralGenerator.generate(1))
+		assert_equal("IV", RomanNumeralGenerator.generate(4))
+		assert_equal("V", RomanNumeralGenerator.generate(5))
+		assert_equal("IX", RomanNumeralGenerator.generate(9))
+		assert_equal("X", RomanNumeralGenerator.generate(10))
+		assert_equal("XL", RomanNumeralGenerator.generate(40))
+		assert_equal("L", RomanNumeralGenerator.generate(50))
+		assert_equal("XC", RomanNumeralGenerator.generate(90))
+		assert_equal("C", RomanNumeralGenerator.generate(100))
+		assert_equal("CD", RomanNumeralGenerator.generate(400))
+		assert_equal("D", RomanNumeralGenerator.generate(500))
+		assert_equal("CM", RomanNumeralGenerator.generate(900))
+		assert_equal("M", RomanNumeralGenerator.generate(1000))
 	end
 end
